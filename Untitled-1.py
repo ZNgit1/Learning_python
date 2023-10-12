d1905c0589b289e8d2bada695efd3adec881b7fc
ZNgit1 <i_am_zn@outlook.com>
2023-10-12 10:58:36 +0300

    Проверка системы для пользователя внутри предприятия

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,6 @@
 import socket
 
-def get_ip_address():
+def get_ip_address():                                 #Вывод ip-адреса компьютера
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     return ip_address
@@ -9,20 +9,20 @@ print("Ваш IP-адрес:" + get_ip_address())
 
 import socket
 
-def get_computer_name():
+def get_computer_name():                              #Вывод имени компьютера  
     return socket.gethostname()
 
 print("Имя Вашего компьютера:" + get_computer_name())
 
 import getpass
 
-def get_username():
+def get_username():                                    #Вывод имени залогированного пользователя
     username = getpass.getuser()
     return username
 
 print("Имя пользователя:" + get_username())
 
-print("По всем вопросам, просьба обращаться: Отдел поддержки пользователей")
+print("По всем вопросам, просьба обращаться: Отдел поддержки пользователей")    #Вывод контактной информации
 print("Тел.: 55555; +7(8617) 602555")
 print("email: 55555@ncsp.com")
 print("https://help.ncsp.ru")
